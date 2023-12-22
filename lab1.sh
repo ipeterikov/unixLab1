8e785a193df067a140caf0f5f50627857eb255df
ipeterikov <113367436+ipeterikov@users.noreply.github.com>
2023-12-22 13:36:38 +0700

    Add files via upload

---
+++ lab1.sh
@@ -0,0 +1,53 @@
+#!/bin/bash -e
+
+deleteTempD() {
+  echo "temp directory deleted"
+  # delete temp directory
+  rm -rf "$temp_dir"
+  exit 1
+}
+
+trap deleteTempD EXIT INT TERM
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <source_file>"
+  exit 1
+fi
+
+source_file="$1"
+
+if [ ! -f "$source_file" ]; then
+  echo "Source file not found: $source_file"
+  exit 1
+fi
+
+# Create a temp directory
+temp_dir=$(mktemp -d)
+if [ ! -d "$temp_dir" ]; then
+  echo "Failed to create a temp directory."
+  deleteTempD
+fi
+
+# Find the output file name in the "&Output:" comment
+output_name=$(grep '&Output:' "$source_file" | awk '{print $2}')
+
+if [ -z "$output_name" ]; then
+  echo "No '&Output:' comment found in the source file."
+  deleteTempD
+fi
+
+# Compile the source file, place output in the tempdir.
+case "$source_file" in
+	*.c)
+	  gcc "$source_file" -o "$temp_dir/$output_name"
+	  ;;
+	*.cpp)
+	  g++ "$source_file" -o "$temp_dir/$output_name"
+	  ;;
+	*)
+	  echo "Unsupported file type! "
+	  exit 3
+esac
+mv "$temp_dir/$output_name" "./$output_name"
+rm -rf "$temp_dir"
+echo "Build successful: &Output: $output_name"
